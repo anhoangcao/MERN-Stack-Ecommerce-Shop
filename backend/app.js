875ff46203b75f05f150c44f42803c59a32eefee
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2023-12-20 14:28:30 +0700

    updates

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const ErrorHandler = require("./middleware/error");
-const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const ErrorHandler = require("./middleware/error");
+
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
